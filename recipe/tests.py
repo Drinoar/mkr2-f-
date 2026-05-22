e6f8494f77823c88979e3572aee7a938b039c8da
Drinoar <80521836+Drinoar@users.noreply.github.com>
2026-05-22 12:46:41 +0300

    unit-тести для категорій

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Category, Recipe
 
-# Create your tests here.
+
+class CategoryModelTest(TestCase):
+
+    def test_create_category(self):
+        category = Category.objects.create(name="Desserts")
+
+        self.assertEqual(category.name, "Desserts")
+        self.assertEqual(str(category), "Desserts")
+
+
